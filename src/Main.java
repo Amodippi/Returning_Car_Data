1644bf1fdbc36c7d18fea2efecf633f589e940cb
Amodippi <w@tgxAeZcS6RYwS>
2023-10-26 19:52:51 +0200

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,109 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+class Car{
+    private String model;
+    private String brand;
+    private int year;
+    private double price;
+    private String color;
+    private int quantity;
+
+    public Car(String model, String brand, int year, double price, String color, int quantity) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.quantity = quantity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public void delivery(int deliveredQuantity) {
+        quantity += deliveredQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    public void sell(int soldQuantity) {
+        if (soldQuantity <= quantity) {
+            quantity -= soldQuantity;
+        } else {
+            System.out.println("Not enough cars in stock to sell.");
+        }
+
+
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Car Car = new Car(" Sedan ", " Toyota ", 2023, 25000.0, " Blue ", 10);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Car.toString());
+
+
+
+        Car.sell(1);
+        System.out.println("Updated Quantity --  " + Car.getQuantity());
+
+
+
     }
 }
